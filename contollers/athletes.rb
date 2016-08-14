92e7b31ef6756f17da926fb37ac67f876490eba4
davidmorrison1981 <david.morrison@geminises.com>
2016-08-14 13:34:41 +0100

    controllers added'

---
+++ contollers/athletes.rb
@@ -0,0 +1,37 @@
+require_relative('../models/athlete')
+
+#index
+get '/athletes' do
+  @athletes = Athlete.all
+  erb(:'athletes/index')
+end
+
+#new
+get '/athletes/new' do
+  erb(:'athletes/new')
+end
+
+#show
+get '/athletes/:id' do
+  @athlete = Athlete.find(params['id'])
+  erb(:'athletes/show')
+end
+
+#edit
+get '/athletes/:id/edit' do
+end
+
+#create
+post '/athletes' do
+  @athlete = Athlete.new(params)
+  @athlete.save
+  redirect to( "athletes" )
+end
+
+#update
+post '/athletes/:id' do
+end
+
+#delete
+delete '/athletes' do
+end
